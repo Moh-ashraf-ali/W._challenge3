8a302d268037fc2b850d4149e36a9c757a08aa23
Moh-ashraf-ali <moh.ashraf.ali2022@gmail.com>
2022-11-07 12:13:50 +0200

    f commit

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+wow = new WOW(
+    {
+    boxClass:     'animate__animated',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+
+  $(document).ready(function(){
+    $(function() {  
+        $("body").niceScroll();
+    });
+    $("#thisdiv").niceScroll({
+      background: "#000000"    })
+  })
